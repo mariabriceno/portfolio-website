1f49b12a79a4d14d8c1d13f3464eb1036ba4405c
Maria Briceno <marilaura06@hotmail.com>
2024-02-11 16:16:28 -0500

    setting background to dark

--- components/Layout.js
+++ components/Layout.js
@@ -15,39 +15,39 @@ import { useEffect } from "react";
 // }
 
 export default function Layout({ children }) {
-  const setAppTheme = () => {
-    const darkMode = localStorage.getItem("theme") === "dark";
-    const lightMode = localStorage.getItem("theme") === "light";
+  // const setAppTheme = () => {
+  //   const darkMode = localStorage.getItem("theme") === "dark";
+  //   const lightMode = localStorage.getItem("theme") === "light";
 
-    if (darkMode) {
-      document.documentElement.classList.add("dark");
-    } else if (lightMode) {
-      document.documentElement.classList.remove("dark");
-    }
-    return;
-  };
+  //   if (darkMode) {
+  //     document.documentElement.classList.add("dark");
+  //   } else if (lightMode) {
+  //     document.documentElement.classList.remove("dark");
+  //   }
+  //   return;
+  // };
 
-  const handleSystemThemeChange = () => {
-    var darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
+  // const handleSystemThemeChange = () => {
+  //   var darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
-    darkQuery.onchange = (e) => {
-      if (e.matches) {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("theme", "dark");
-      } else {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("theme", "light");
-      }
-    };
-  };
+  //   darkQuery.onchange = (e) => {
+  //     if (e.matches) {
+  //       document.documentElement.classList.add("dark");
+  //       localStorage.setItem("theme", "dark");
+  //     } else {
+  //       document.documentElement.classList.remove("dark");
+  //       localStorage.setItem("theme", "light");
+  //     }
+  //   };
+  // };
 
-  useEffect(() => {
-    setAppTheme();
-  }, []);
+  // useEffect(() => {
+  //   setAppTheme();
+  // }, []);
 
-  useEffect(() => {
-    handleSystemThemeChange();
-  }, []);
+  // useEffect(() => {
+  //   handleSystemThemeChange();
+  // }, []);
 
   return (
     <div className="mx-auto grid grid-cols-12 h-screen p-24">{children}</div>
--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
       <Html lang="en" className="theme-compiled">
         <Head />
         <body
-          className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base h-full overflow-hidden`}
+          className={`antialiased text-lg bg-gray-900 text-white leading-base h-full overflow-hidden`}
         >
           <Main />
           <NextScript />
